f8b0217dcdc86f3792f875badf3047fc43eac534
Julian Krzefski <krzefski@heptacom.de>
2026-04-16 17:45:49 +0200

    Add error-handling to AccountLoginAction

--- src/Http/StoreApi/Action/AccountLoginAction.php
+++ src/Http/StoreApi/Action/AccountLoginAction.php
@@ -6,6 +6,7 @@ namespace Heptacom\HeptaConnect\Package\Shopware6\Http\StoreApi\Action;
 
 use Heptacom\HeptaConnect\Package\Shopware6\Http\StoreApi\Action\Contract\AccountLogin\AccountLoginActionInterface;
 use Heptacom\HeptaConnect\Package\Shopware6\Http\StoreApi\Action\Contract\AccountLogin\AccountLoginCredentials;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\StoreApi\ErrorHandling\Exception\CustomerAuthBadCredentialsException;
 
 final class AccountLoginAction extends AbstractActionClient implements AccountLoginActionInterface
 {
@@ -18,6 +19,55 @@ final class AccountLoginAction extends AbstractActionClient implements AccountLo
 
         $response = $this->sendAuthenticatedRequest($request);
 
-        return $response->getHeaderLine('sw-context-token');
+        if ($response->getStatusCode() === 200) {
+            $contextToken = $response->getHeaderLine('sw-context-token');
+
+            if ($contextToken === '') {
+                throw new CustomerAuthBadCredentialsException(
+                    $request,
+                    $response,
+                    'Missing sw-context-token in response',
+                    1776258143,
+                );
+            }
+
+            return $contextToken;
+        }
+
+        try {
+            $responseData = \json_decode(
+                (string) $response->getBody(),
+                true,
+                flags: \JSON_THROW_ON_ERROR,
+            );
+        } catch (\JsonException) {
+            throw new CustomerAuthBadCredentialsException(
+                $request,
+                $response,
+                'Response body is malformed',
+                1776337962,
+            );
+        }
+
+        $errors = $responseData['errors'] ?? [];
+
+        if ($errors === [] || !\is_array($errors)) {
+            throw new CustomerAuthBadCredentialsException(
+                $request,
+                $response,
+                'Response body is missing errors',
+                1776338023,
+                new \Exception((string) $response->getBody(), $response->getStatusCode()),
+            );
+        }
+
+        $error = $errors[0];
+
+        throw new CustomerAuthBadCredentialsException(
+            $request,
+            $response,
+            (string) $error['detail'],
+            (int) $error['status'],
+        );
     }
 }
--- src/Http/StoreApi/Action/Contract/AccountLogin/AccountLoginActionInterface.php
+++ src/Http/StoreApi/Action/Contract/AccountLogin/AccountLoginActionInterface.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Package\Shopware6\Http\StoreApi\Action\Contract\AccountLogin;
 
+use Heptacom\HeptaConnect\Package\Shopware6\Http\StoreApi\ErrorHandling\Exception\CustomerAuthBadCredentialsException;
+
 interface AccountLoginActionInterface
 {
+    /**
+     * @throws CustomerAuthBadCredentialsException
+     */
     public function login(AccountLoginCredentials $credentials): string;
 }
