9e4c71c858cdb0b1cbf28f742b8c03fad807833b
Julian Krzefski <krzefski@heptacom.de>
2026-04-16 17:46:22 +0200

    Add alias `PortalStorageInterface` back into container after removing it previously

--- src/Shopware6Package.php
+++ src/Shopware6Package.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Heptacom\HeptaConnect\Package\Shopware6;
 
 use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PackageContract;
+use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalStorageInterface;
+use Psr\SimpleCache\CacheInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 if (!\class_exists(PackageContract::class)) {
     // `composer require heptacom/heptaconnect-portal-base` to add ability to act as HEPTAconnect package for good integration into portals https://heptaconnect.io/
@@ -13,4 +16,12 @@ if (!\class_exists(PackageContract::class)) {
 
 final class Shopware6Package extends PackageContract
 {
+    public function buildContainer(ContainerBuilder $containerBuilder): void
+    {
+        parent::buildContainer($containerBuilder);
+
+        $containerBuilder->addAliases([
+            PortalStorageInterface::class => CacheInterface::class,
+        ]);
+    }
 }
